b88993fdfdd77311024aff2d244e0c841bc17e5b
김규용 <42133539+givea04291@users.noreply.github.com>
2020-04-12 23:50:29 +0900

    Update catbot.py

--- catbot.py
+++ catbot.py
@@ -15,7 +15,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-#인사 and 일상대화
+#인사
     if message.content.startswith("냥이야 안녕"):
         await message.channel.send("다음부턴 고양이말로 인사해라냥")
 
@@ -24,9 +24,11 @@ async def on_message(message):
 
     if message.content.startswith("냥이야 반가워"):
         await message.channel.send("나도 반갑다냥")
-
-    if message.content.startswith("냥이야 뭐해"):
-        await message.channel.send("할게 없어서 아무것도 안하고있다냥 심심하다냥")
+#중1
+    if message.content.startswith("냥이야 거듭제곱"):
+        embed = (color=0x00D8FF)
+        embed.set_image(url="https://media.discordapp.net/attachments/698830342458703912/698905428008108073/2e951eacfe5b6cfc.png")
+        await message.channel.send("같은 수나 문자를 거듭하여 곱한 것", embed=embed)
 
 
 
