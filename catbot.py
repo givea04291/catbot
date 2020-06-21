aed2dab5b328d6c3bff0aa58a1ca75db668cc43b
김규용 <42133539+givea04291@users.noreply.github.com>
2020-06-21 13:21:31 +0900

    Update catbot.py

--- catbot.py
+++ catbot.py
@@ -14,11 +14,45 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.endswith(f'{prefix} 안녕'):
-        await message.channel.send('그래 안녕')
+    # 수학
+    if message.content.endswith(f'{prefix} 가감'):
+        await message.channel.send('(1) 더하거나 뺴는 일. 또는 그렇게 하여 알맞게 맞추는 일.')
+        await message.channel.send('(2) 덧셈과 뺄셈을 아울러 이르는 말.')
 
-    if message.content.endswith(f'{prefix} 안녕하세요'):
-        await message.channel.send('그래 안녕허허허°')
+    if message.content.endswith(f'{prefix} 가감법'):
+        await message.channel.send('(1) 덧셈과 뺄셈을 하는 방법.')
+        await message.channel.send('(2) 두 개 이상의 미지수를 가진 연립 방정식에서 한 미지수의 계수를 곱셈이나 나눗셈을 써서 같게 만든 후, 더하거나 빼어 그 미지수를 없애는 '
+                                   '방법.')
+
+    if message.content.endswith(f'{prefix} '):
+        await message.channel.send('')
+
+    if message.content.endswith(f'{prefix} '):
+        await message.channel.send('')
+
+    if message.content.endswith(f'{prefix} '):
+        await message.channel.send('')
+
+    if message.content.endswith(f'{prefix} '):
+        await message.channel.send('')
+
+    if message.content.endswith(f'{prefix} '):
+        await message.channel.send('')
+
+    if message.content.endswith(f'{prefix} '):
+        await message.channel.send('')
+
+    if message.content.endswith(f'{prefix} '):
+        await message.channel.send('')
+
+    if message.content.endswith(f'{prefix} '):
+        await message.channel.send('')
+
+    if message.content.endswith(f'{prefix} '):
+        await message.channel.send('')
+
+    if message.content.endswith(f'{prefix} '):
+        await message.channel.send('')
 
 
 access_token = os.environ["BOT_TOKEN"]
