76dcd663f96c692fb0fa33c91c3350563a7c5c69
김규용 <42133539+givea04291@users.noreply.github.com>
2020-09-11 18:49:38 +0900

    Update catbot.py

--- catbot.py
+++ catbot.py
@@ -1,16 +1,16 @@
 import discord
-from discord.ext import commands
 import os
 
-bot = commands.Bot(command_prefix='냥이야')
+client = discord.Client()
 
-@bot.event
+@client.event
 async def on_ready():
     print('complete')
     print('------')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@client.event
+async def on_message(message):
+    if message.content.startswith('!ping'):
+        await message.channel.send('pong')
 
-bot.run(os.environ["BOT_TOKEN"])
+client.run(os.environ["BOT_TOKEN"])
